04c28d080f9c96c77c6d08fd83801056bd322672
NICOLE SIQUEIRA DORTA <54233987898@info.local>
2024-05-13 08:30:20 -0300

    feat: exercício 7

--- ex7.js
+++ ex7.js
@@ -8,20 +8,23 @@ Valor total da compra de R$ 100,01 a R$ 200,00: desconto de 10%.
 Valor total da compra acima de R$ 200,00: desconto de 20%.
 Exiba o valor final da compra com o desconto aplicado.*/
 
-let valorCompra = prompt ("Digite o valor total da compra: ");
+let valorTotal = parseFloat(prompt ("Digite o valor total da compra: "));
 
-valorCompra = parseFloat(valorCompra);
+let desconto;
 
-let valorFinal;
-
-if (valorCompra <= 100) {
-    valorFinal = valorCompra;
+if (valorTotal <= 100) {
+    desconto = 0;
+    console.log("O valor de desconto foi: " + desconto);
 }
-else if (valorCompra <= 200) {
-    valorFinal = valorCompra * 0.9;
+else if (valorTotal <= 200) {
+    desconto = 0.1;
+    console.log("O valor de desconto foi: " + (desconto * 100) + "%");
 }
 else {
-    valorFinal = valorCompra * 0.8;
+    desconto = 0.2;
+    console.log("O valor de desconto foi: " + (desconto * 100) + "%");
 }
 
-console.log("O total da sua compra com desconto é de: R$" +  valorFinal.toFixed(2));
+let valorFinal = valorTotal - (valorTotal * desconto);
+
+console.log("O valor final da compra com o desconto é de: R$" + valorFinal.toFixed(2));
