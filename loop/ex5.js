957e682dac84b41bb3ea863dfa54d061f18fd402
NICOLE SIQUEIRA DORTA <54233987898@info.local>
2024-05-14 09:58:13 -0300

    feat: exercício 6

---
+++ loop/ex5.js
@@ -0,0 +1,4 @@
+// 5. Imprimir os primeiros 10 números da sequência de Fibonacci.
+// "1,1,2,3,5,8,13,21,34,55"
+// fn= fn − 1 + fn − 2
+
---
+++ loop/ex6.js
@@ -0,0 +1,11 @@
+// WHILE
+
+//6. Imprimir números pares de 0 a 20 utilizando o loop while 
+
+let numero = 0;
+
+while (numero <= 18){
+    numero +=2
+    console.log(numero);
+}
+
