e075bdfe4e0cbf20e5353e7e3107dc98f7bcf0c2
NICOLE SIQUEIRA DORTA <54233987898@info.local>
2024-05-14 09:28:43 -0300

    feat: exercício 4

---
+++ loop/ex4.js
@@ -0,0 +1,8 @@
+/* 4. Faça um programa que imprima na tela a tabuada de um número
+qualquer escolhido pelo usuário até o 10. */
+
+let numero = prompt("Digite um número e veja sua tabuada até o 10: ");
+
+for(let i = 0; i <= 10; i++){
+    console.log(numero * i);
+}
