a432982958e5ef73cc5aeee706adddc59884fa0d
NICOLE SIQUEIRA DORTA <54233987898@info.local>
2024-05-17 10:51:51 -0300

    feat: DOM - exemplo

---
+++ dom/script.js
@@ -0,0 +1,7 @@
+document.body.innerHTML = '<h1 id = "titulo">Título Legal</h1>'
+
+let titulo = document.getElementById("titulo")
+
+titulo.style.fontFamily = "Segoe UI"
+
+titulo.style.color = "red"
